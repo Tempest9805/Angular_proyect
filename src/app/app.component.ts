210596a070d5ef02f3afd5528972c57c673b756a
Tempest9805 <111609578+Tempest9805@users.noreply.github.com>
2024-10-29 13:22:08 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Angular App';
+  public counter: number = 10;
+
+  increaseBy(value: number):void {
+    this.counter += value;
+  }
+  decreaseBy(value: number):void {
+    this.counter -= value;
+  }
+  reset():void {
+    this.counter = 10;
+  }
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-counter',
+    template: '<h1>hi</h1>'
+})
+export class CounterComponent{
+
+}
