471ced6df1af958e6d7b4a7b9e5e61c52a4f079d
Tempest9805 <111609578+Tempest9805@users.noreply.github.com>
2024-10-30 17:21:33 -0600

    update

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-counter',
-    template: '<h1>hi</h1>'
+    template: `
+        <h3>Counter: {{ counter }}</h3>
+        <button (click)="increaseBy(+1)">+1</button>
+        <button (click)="decreaseBy(+1)">-1</button>
+        <button (click)="reset()">reset</button>
+    `
 })
-export class CounterComponent{
+
+export class CounterComponent {
+    constructor() { }
+
+    public counter: number = 10;
+
+    increaseBy(value: number):void {
+        this.counter += value;
+    }
+    decreaseBy(value: number):void {
+        this.counter -= value;
+    }
+    reset():void {
+        this.counter = 10;
+    }
 
 }
